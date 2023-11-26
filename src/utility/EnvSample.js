8114b9329065bbbc499651a69c7954580e6ddcd2
monil-ia <monil.nariya@infiniteai.in>
2023-11-26 20:36:31 +0530

    env sample gennerate

---
+++ src/utility/EnvSample.js
@@ -0,0 +1,11 @@
+import fs from 'fs';
+// import path from "path";
+
+fs.watch('.env', (eventType) => {
+    if (eventType === 'change') {
+        const env = fs.readFileSync('.env', 'utf-8');
+        const sample = env.replace(/=.*/g, '= " "'); // Replace all values with an empty string
+        fs.writeFileSync('.env.sample', sample);
+    }
+});
+
