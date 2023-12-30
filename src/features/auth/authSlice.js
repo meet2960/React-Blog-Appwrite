08446bcf1332640ea9ceb19188d0d58a422d55af
meetentrib <mghelani@entrib.com>
2023-12-30 12:32:22 +0530

    minor bug fixes

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,30 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit';
-import LogHelper from '@/utility/LogHelper';
-const storedUserData = JSON.parse(localStorage.getItem('userData'));
+import { createSlice } from '@reduxjs/toolkit'
+import LogHelper from '@/utility/LogHelper'
+const storedUserData = JSON.parse(localStorage.getItem('userData'))
 
 const initialState = {
   status: storedUserData ? true : false,
   userData: storedUserData || null
-};
+}
 
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
     login: (state, action) => {
-      state.status = true;
-      state.userData = action.payload;
-      localStorage.setItem('userData', JSON.stringify(action.payload));
+      state.status = true
+      state.userData = action.payload
+      localStorage.setItem('userData', JSON.stringify(action.payload))
     },
     logout: (state, action) => {
-      LogHelper.log('action', action);
-      state.status = false;
-      state.userData = null;
-      localStorage.removeItem('userData');
+      LogHelper.log('action', action)
+      state.status = false
+      state.userData = null
+      localStorage.removeItem('userData')
     }
   }
-});
+})
 
-export const { login, logout } = authSlice.actions;
+export const { login, logout } = authSlice.actions
 
-export default authSlice.reducer;
+export default authSlice.reducer
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,40 +1,7 @@
-// import { Container } from "react-bootstrap";
 import { Header, Footer } from '../components/index';
 import { Outlet } from 'react-router-dom';
-// import { useDispatch } from "react-redux";
-import { useState } from 'react';
-import { useEffect } from 'react';
-const Layout = () => {
-  const [loading, setLoading] = useState(true);
-  // const dispatch = useDispatch();
-  useEffect(() => {
-    // authService
-    //   .getCurrentUser()
-    //   .then((userData) => {
-    //     if (userData) {
-    //       dispatch(login(userData));
-    //     } else {
-    //       console.log("Error WHile Loggin");
-    //       dispatch(logout());
-    //     }
-    //   })
-    //   .catch(() => {
-    //     console.log("Error While Calling Login API");
-    //     dispatch(logout());
-    //   })
-    //   .finally(() => {
-    //     setLoading(false);
-    //   });
-    setLoading(false);
-  }, []);
 
-  if (loading) {
-    return (
-      <div>
-        <h1>Login.....</h1>
-      </div>
-    );
-  }
+const Layout = () => {
   return (
     <div className='wrapper'>
       <Header />
